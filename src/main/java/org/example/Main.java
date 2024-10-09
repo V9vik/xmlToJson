60f5f9ecb8dd1b44811600bae3dcc745a92d8d41
V9va <myzikov03@gmail.com>
2024-10-10 00:00:25 +0300

    Inital commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,11 +7,18 @@ import com.opencsv.CSVReader;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -27,10 +34,55 @@ public class Main {
         if (list != null) {
             String json = listToJson(list);
             System.out.println(json);
-            writeString(json, "output.json");
+            writeString(json, "data.json");
         } else {
             System.out.println("Ошибка при чтении CSV файла.");
         }
+
+        String fileNameSecond = "data.xml";
+        List<Employee> list2 = parseXML(columnMapping, fileNameSecond);
+        if(list2!=null) {
+            String json2 = listToJson(list2);
+            System.out.println(json2);
+            writeString(json2, "data2.json");
+        }else {
+            System.out.println("Ошибка при чтении CSV файла.");
+
+        }
+    }
+
+    private static List<Employee> parseXML(String[] columnMapping, String fileName) {
+        List<Employee> employees = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(fileName);
+
+            Node root = document.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node instanceof Element) {
+                    Element element = (Element) node;
+
+                    // Предполагается, что структура XML соответствует полям Employee
+                    int id = Integer.parseInt(element.getElementsByTagName("id").item(0).getTextContent());
+                    String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = element.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+
+                    Employee employee = new Employee(id, firstName, lastName, country, age);
+                    employees.add(employee);
+                }
+            }
+        } catch (Exception e) {
+
+
+            e.printStackTrace();
+        }
+        return employees;
     }
 
     public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
